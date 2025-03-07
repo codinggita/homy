7a2f975cca2e3787f7383ca2343fedb9014848a1
Jatin <jatinrajvani.cg@gmail.com>
2025-03-07 18:13:24 +0530

    Adding a page for login means when user click on login so it got two option Login by auth and Login By Jwt

--- Frontend/vite_app/src/Components/LogIn.jsx
+++ Frontend/vite_app/src/Components/LogIn.jsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css"
 const Login=()=>{
+    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+    const navigate = useNavigate();
+
+    const handleSearch=()=>{
+        navigate('/hostel')
+    };
+
     return(<>
-     <div className="login">
-        <h1>Login To Your Account</h1>
-        Hello , I am a login page
+    <div className="maindiv">
+    <div className="loginbyauth">
+    <div className="auth-buttons">
+            {isAuthenticated && (
+              <p className="user-welcome">Welcome, {user.name}</p>
+              
+            )}
+            
+             {/* {console.log(user)} */}
+            {isAuthenticated ? (
+              <button 
+                onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} 
+                className="logout-button"
+              >
+                Log Out
+              </button>
+            ) : (
+              <button onClick={loginWithRedirect} className="login-button">
+                Log In
+              </button>
+              
+            )}
+          </div>
      </div>
+
+     <div className="loginbyjwt">
+       <button onClick={handleSearch} className="newbutton">click me</button>
+     </div>
+
+
+    </div>
+
     </>)
 };
 export default Login;
